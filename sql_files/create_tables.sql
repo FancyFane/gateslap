40db0c22bd0012c16cba251042e5c48f5d5ad5a1
FancyFane <fane@planetscale.com>
2021-12-15 14:50:22 -0600

    Ensure sql_files does not conflict with vitess examples
    Signed-off-by: FancyFane <fane@planetscale.com>

--- sql_files/create_tables.sql
+++ sql_files/create_tables.sql
@@ -1,3 +1,3 @@
-create table product (sku varbinary(128), description varbinary(128), price bigint, primary key(sku));
-create table customer (customer_id bigint not null auto_increment, email varbinary(128), primary key(customer_id));
-create table corder (order_id bigint not null auto_increment, customer_id bigint, sku varbinary(128), price bigint, primary key(order_id));
+create table product2 (sku varbinary(128), description varbinary(128), price bigint, primary key(sku));
+create table customer2 (customer_id bigint not null auto_increment, email varbinary(128), primary key(customer_id));
+create table corder2 (order_id bigint not null auto_increment, customer_id bigint, sku varbinary(128), price bigint, primary key(order_id));
