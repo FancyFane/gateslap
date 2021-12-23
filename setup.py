8f142654e782d68b284150207422aae2d3f4ff36
FancyFane <fane@planetscale.com>
2021-12-23 11:51:02 -0600

    Adding psutil to setup.py
    Signed-off-by: FancyFane <fane@planetscale.com>

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='gateslap',
-    version='0.4.0',
+    version='0.4.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'PyMySQL','DBUtils','tqdm'
+        'PyMySQL','DBUtils','tqdm','psutil'
     ],
     description = ("A utility designed to generate traffic for VTGate."),
     keywords = "mysql vtgate vitess traffic SQL",
